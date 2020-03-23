827b4e5e470c5eecb3ba2ec9cd8109b4192d088e
Andrea <andreademarco91@gmail.com>
2020-03-23 16:23:52 +0100

    DB OK

--- resources/views/guitars/index.blade.php
+++ resources/views/guitars/index.blade.php
@@ -5,6 +5,16 @@
     <title></title>
   </head>
   <body>
-    {{dd($guitars)}}
+    <a href="{{Route('guitars.create')}}">INSERISCI ANNUNCIO</a>
+    @foreach ($guitars as $guitar)
+      <ul>
+        <li>{{$guitar['brand']}}</li>
+        <li>{{$guitar['model']}}</li>
+        <li>{{$guitar['year']}}</li>
+        <li>{{$guitar['color']}}</li>
+        <li>{{$guitar['price']}}</li>
+        <li>{{$guitar['description']}}</li>
+      </ul>
+    @endforeach
   </body>
 </html>
